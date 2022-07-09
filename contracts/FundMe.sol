ac650fa634c0a243b62336179d28fac88c9ae304
hrugved <whrugved@gmail.com>
2022-07-09 13:36:14 +0530

    Add initial deployments

---
+++ contracts/FundMe.sol
@@ -0,0 +1,57 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.8;
+
+import "./PriceConverter.sol";
+
+error NotOwner();
+
+contract FundMe {
+  using PriceConverter for uint256;
+
+  uint256 public constant MIN_USD = 50 * 1e18;
+  address[] public funders;
+  mapping(address => uint256) public addressToAmountFunded;
+  address public immutable i_owner;
+  AggregatorV3Interface public priceFeed;
+
+  constructor(address priceFeedAddress) {
+    i_owner = msg.sender;
+    priceFeed = AggregatorV3Interface(priceFeedAddress);
+  }
+
+  modifier onlyOwner() {
+    if (msg.sender != i_owner) {
+      revert NotOwner();
+    }
+    _;
+  }
+
+  function fund() public payable {
+    require(
+      msg.value.getConversionRate(priceFeed) > MIN_USD,
+      "amount lesser than required minimum"
+    );
+    funders.push(msg.sender);
+    addressToAmountFunded[msg.sender] += msg.value;
+  }
+
+  function withdraw() public onlyOwner {
+    for (uint256 funderIndex = 0; funderIndex < funders.length; funderIndex++) {
+      address funder = funders[funderIndex];
+      addressToAmountFunded[funder] = 0;
+    }
+    funders = new address[](0);
+    (bool callSuccess, ) = payable(msg.sender).call{
+      value: address(this).balance
+    }("");
+    require(callSuccess, "call failed");
+  }
+
+  receive() external payable {
+    fund();
+  }
+
+  fallback() external payable {
+    fund();
+  }
+}
---
+++ helper-hardhat-config.js
@@ -0,0 +1,17 @@
+const networkConfig = {
+  5: {
+    name: "goerli",
+    ethUsdPriceFeed: "0x9326BFA02ADD2366b30bacB125260Af641031331",
+  },
+}
+
+const devChains = ['hardhat','localhost']
+const DECIMALS = 8
+const INITIAL_ANSWER = 2000
+
+module.exports = {
+  networkConfig,
+  devChains,
+  DECIMALS,
+  INITIAL_ANSWER
+}
